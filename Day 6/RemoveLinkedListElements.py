d7702d05aeddf974db79303ad21bc57ea8a90e67
AKasem1 <abdelrahmankasem7@gmail.com>
2025-09-06 23:33:58 +0300

    Day 6

---
+++ Day 6/RemoveLinkedListElements.py
@@ -0,0 +1,9 @@
+#https://neetcode.io/problems/remove-linked-list-elements?list=neetcode250
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
